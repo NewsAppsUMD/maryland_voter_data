1ea6da8a282a9c99592b1ab901b817d536bc92b2
Geoffrey Zhang <50123131+geozha583@users.noreply.github.com>
2025-05-09 01:42:22 +0000

    Deploy frozen build

--- voters/md_voter_file.txt/cleaned_mdagg.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('agg_MD_GenderSplit_County.csv', index_col=0)
-df_No_2016 = df.drop('voted_2016', axis='columns', inplace =True)
-df.to_csv('20-24_agg_MD.csv', index=False)
-
-
-
